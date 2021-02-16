f85da2eb11975ed156b6ddd51f8aeb507422c7b2
Dan Buchholz <db@Dans-MacBook-Air.local>
2021-02-16 16:29:17 -0600

    Feat: Added user query form
    Created a form that allows users to input the 'fromBlock' and 'toBlock', which fetches the number of births from the 'CryptoKitties' contract and returns the 'numberOfBirthedKitties' during that period.

---
+++ src/components/QueryForm.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+
+class QueryForm extends Component {
+	render() {
+		return (
+			<div className='container-fluid mt-5'>
+				<div className='row'>
+					<div className='content mr-auto ml-auto'>
+						<h3 className='text-center'>Query Form:</h3>
+						<form
+							onSubmit={async (event) => {
+								event.preventDefault();
+								const fromBlock = this.fromBlock.value;
+								const toBlock = this.toBlock.value;
+								let birthedKittiesArray = await this.props.queryCryptoKitties(
+									this.props.cryptoKittiesContract,
+									fromBlock,
+									toBlock
+								);
+
+								this.props.queryCryptoKittiesStateHandler([
+									fromBlock,
+									toBlock,
+									birthedKittiesArray,
+								]);
+							}}
+						>
+							<div className='form-row'>
+								<div className='form-group col-md-6'>
+									<label htmlFor='fromBlock'>
+										Starting Block
+									</label>
+									<input
+										className='form-control'
+										id='fromBlock'
+										type='text'
+										ref={(input) => {
+											this.fromBlock = input;
+										}}
+										placeholder='Starting block...'
+										required
+									/>
+								</div>
+								<div className='form-group col-md-6'>
+									<label htmlFor='toBlock'>Ending Block</label>
+									<input
+										className='form-control'
+										id='toBlock'
+										type='text'
+										ref={(input) => {
+											this.toBlock = input;
+										}}
+										placeholder='Ending block...'
+										required
+									/>
+								</div>
+							</div>
+							<div className='form-row'>
+								<div className='form-group col text-center'>
+									<button
+										type='submit'
+										className='btn btn-primary'
+									>
+										Query
+									</button>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default QueryForm;
---
+++ src/components/QueryResults.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class QueryResults extends Component {
+	render() {
+		return (
+			<div className='container-fluid mt-5'>
+				<div className='row'>
+					<div className='content mr-auto ml-auto'>
+						<h3 className='text-center'>Results:</h3>
+						<table>
+							<tbody>
+								<tr>
+									<th>Start Block</th>
+									<th>End Block</th>
+									<th>Total Number of Births</th>
+								</tr>
+								<tr>
+									<td>{this.props.fromBlock}</td>
+									<td>{this.props.toBlock}</td>
+									<td>{this.props.numberOfBirthedKitties}</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default QueryResults;
