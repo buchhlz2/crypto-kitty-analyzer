3b1fac6b60531f3c4417d26af0eef136c3e21046
Dan Buchholz <db@Dans-MacBook-Air.local>
2021-02-17 20:02:07 -0600

    Refactor: State flow with 'queryCryptoKitties' & handler
    Fixed some logic issues where state was being passed down to 'QueryForm' and function evaluation were more complicated than needed. However, the query result size issue persists and needs to be fixed, but there are unknown reasons for why 'queryCryptoKitties' doesn't work -- aside from the Infura 10k query limit

--- src/components/QueryResults.js
+++ src/components/QueryResults.js
@@ -7,22 +7,24 @@ class QueryResults extends Component {
 				<div className='row'>
 					<div className='content mr-auto ml-auto'>
 						<h3 className='text-center'>Results:</h3>
-						<table>
-							<tbody>
-								<tr>
-									<th>Start Block</th>
-									<th>End Block</th>
-									<th>Total Number of Births</th>
-									<th>Matron ID</th>
-								</tr>
-								<tr>
-									<td>{this.props.fromBlock}</td>
-									<td>{this.props.toBlock}</td>
-									<td>{this.props.numberOfBirthedKitties}</td>
-									<td>{this.props.matronWithMaxBirths}</td>
-								</tr>
-							</tbody>
-						</table>
+						<div className='table-responsive'>
+							<table className='table'>
+								<tbody>
+									<tr>
+										<th scope='col'>Start Block</th>
+										<th scope='col'>End Block</th>
+										<th scope='col'>Total Number of Births</th>
+										<th scope='col'>Matron ID</th>
+									</tr>
+									<tr>
+										<td>{this.props.fromBlock}</td>
+										<td>{this.props.toBlock}</td>
+										<td>{this.props.numberOfBirthedKitties}</td>
+										<td>{this.props.matronWithMaxBirths}</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
 					</div>
 				</div>
 			</div>
